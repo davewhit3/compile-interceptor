4e861dd9be6dcc5f855658936bf4fe1a421d8238
Dawid Bialy <dawid.bialy@wbdcontractor.com>
2026-04-17 13:45:28 +0200

    feat: Add regex pattern matching for version-agnostic transformations
    Problem:
    
    - Hardcoded version in valkey transformer (@v1.0.73) caused build failures
    
      when users upgraded to newer versions
    
    - Users had to wait for interceptor updates or downgrade dependencies
    
    Solution:
    
    - Added optional SourceFilePattern field to transformer struct
    
    - Pattern matching using regexp for flexible version handling
    
    - Updated valkey transformer to match any version using pattern:
    
      github.com/valkey-io/valkey-go@v[\d.]+/client.go
    
    - Backward compatible: existing transformers work unchanged
    
    Benefits:
    
    - Automatic support for all valkey-go versions
    
    - No interceptor updates needed when dependencies update
    
    - Extensible pattern matching for other transformers
    
    Testing:
    
    - Tested with valkey-go v1.0.73, v1.0.74, v1.0.75
    
    - Backward compatible with existing http transformer

--- transform/valkey.go
+++ transform/valkey.go
@@ -1,16 +1,33 @@
 package transform
-import (_ "embed"; "regexp")
+
+import (
+	_ "embed"
+	"regexp"
+)
+
 //go:embed templates/valkey.go.tpl
 var templateValkeySourceCode string
+
+// valkeyFilePattern matches valkey-go client.go with any version
+// Examples it will match:
+//   - github.com/valkey-io/valkey-go@v1.0.73/client.go
+//   - github.com/valkey-io/valkey-go@v1.0.74/client.go
+//   - github.com/valkey-io/valkey-go@v2.5.1/client.go
 var valkeyFilePattern = regexp.MustCompile(`github\.com/valkey-io/valkey-go@v[\d.]+/client\.go$`)
+
 func init() {
-	Register(&valkeyTransformer{transformer: &transformer{
-		SourcePackage: "github.com/valkey-io/valkey-go",
-		SourceFile: "client.go",
-		SourceFilePattern: valkeyFilePattern,
-		TemplateCode: templateValkeySourceCode,
-		TargetFunc: "Do",
-		Imports: []string{`"fmt"`},
-	}})
+	Register(&valkeyTransformer{
+		transformer: &transformer{
+			SourcePackage:     "github.com/valkey-io/valkey-go",
+			SourceFile:        "client.go",
+			SourceFilePattern: valkeyFilePattern,
+			TemplateCode:      templateValkeySourceCode,
+			TargetFunc:        "Do",
+			Imports:           []string{`"fmt"`},
+		},
+	})
 }
-type valkeyTransformer struct { *transformer }
+
+type valkeyTransformer struct {
+	*transformer
+}
